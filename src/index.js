6a7de15a09c111eec379036055ce139ad2080eed
mgoncalves3 <55608718+mgoncalves3@users.noreply.github.com>
2022-09-06 12:33:39 +0100

    added del main fn

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './reset.css';
 import './style.css';
+
+function resetMain() {
+  let mainDiv = document.querySelector('main');
+  (function deleteElements() {
+    while (mainDiv.hasChildNodes()) {
+      mainDiv.removeChild(mainDiv.lastChild)
+    };
+  })();
+};
+
+resetMain();
